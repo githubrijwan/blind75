25b8a5d3ee3ff605b7f5593d7a118d011d1c87e8
Rijwan Desai <learner@Rijwans-MBP.hsd1.ca.comcast.net>
2023-03-14 20:52:41 -0700

    adding first program

--- 1.two_sum.py
+++ 1.two_sum.py
@@ -1,15 +1,15 @@
 def two_sum(lst,target):
     map = {}
     
-    for i,n enumerate(lst):
+    for i,n in enumerate(lst):
         diff = target - n
         
         if diff in map:
             return [map[diff], i]
-        map[n] = I
+        map[n] = i
     return [0, 0]    
     
-    
-   
-    
+
+if __name__ == "__main__":
+    print(two_sum([1,4,3,5,7], 6))    
     
